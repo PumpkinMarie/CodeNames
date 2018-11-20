966e98750d030fc69e423c97315d65c88a8a807c
Robin Biscay <biscay.rob@gmail.com>
2018-11-20 16:05:02 +0100

    add translation on json file

--- affichage.js
+++ affichage.js
@@ -1,5 +1,5 @@
-class Board{
+class Board {
   this.canvas = $('#canvas')[0];
-  this.ctx = canvas.getContext('2d');
-  this.ctx2 = document.getElementById('canvas2').getContext('2d');
+this.ctx = canvas.getContext('2d');
+this.ctx2 = document.getElementById('canvas2').getContext('2d');
 }
