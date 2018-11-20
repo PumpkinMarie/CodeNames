54995668603114cdfa2820ff9054d44289e59969
Azertui <rauch.arthur@gmail.com>
2018-11-20 10:56:46 +0100

    Add team functions

--- team.js
+++ team.js
@@ -1,6 +1,20 @@
 class Team{
-
-	constructor(){
-		
+	let score=0;
+	constructor(color,name,NbPlayers){
+		this.color=color;
+		this.name=name;
+		this.NbPlayers=NbPlayers;
+	}
+	getScore(){
+		return this.score;
+	}
+	AddScore(points){
+		this.score+=points;
+	}
+	getName(){
+		return this.name;
+	}
+	getColor(){
+		return this.color; //Blue, Red, Grey, Black
 	}
 }
