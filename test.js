32ff503a8d8d8b1e774a83b4565f85773e5af1aa
Robin Biscay <biscay.rob@gmail.com>
2018-11-20 23:35:46 +0100

    lang.js can now word by language

--- test.js
+++ test.js
@@ -1,12 +1,11 @@
-window.onload = function() {
+window.onload = function () {
 	//On teste le code du fichier board
-	let board=new Board();
-	let message="";
-	for(let i=0;i<5;i++){
-		for(let j=0;j<5;j++){
-			message+=board.CardBoard[i][j].team+" ";
+	let board = new Board();
+	for (let i = 0; i < 5; i++) {
+		let message = "";
+		for (let j = 0; j < 5; j++) {
+			message += board.CardBoard[i][j].team + " ";
 		}
 		console.log(message);
-		message="";
 	}
 }
