d2aab97cf9e89c07776af0af9b3ac07b204ebae1
Robin Biscay <biscay.rob@gmail.com>
2018-11-20 10:23:57 +0100

    add noyau functions file

---
+++ noyau.js
@@ -0,0 +1,50 @@
+class noyau {
+
+	noyau(mode) {
+
+	}
+
+	setTeam1() {
+
+	}
+
+	setTeam2() {
+
+	}
+
+	getBoardState() {
+
+	}
+
+	createBoard() {
+
+	}
+
+	isBleuTeam() {
+
+	}
+
+	setMasterSelection(cardArray) {
+
+	}
+
+	verifySpyCard() {
+
+	}
+
+	askEndTour() {
+
+	}
+
+	endTour() {
+
+	}
+
+	getScore() {
+
+	}
+
+	isEndGame() {
+
+	}
+}
