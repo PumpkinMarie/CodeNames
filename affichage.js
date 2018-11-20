149dbd5296c68fad75b0f7ac62fafa224f2bae68
MONFOUGA MARIE <marie.monfouga@gmail.com>
2018-11-20 21:45:18 +0100

    affichage debug

--- affichage.js
+++ affichage.js
@@ -1,5 +1,9 @@
-class Board {
-  this.canvas = $('#canvas')[0];
-this.ctx = canvas.getContext('2d');
-this.ctx2 = document.getElementById('canvas2').getContext('2d');
+function Afficher_Langues(){
+    $('#langues').css("visibility","visible");
+    $('#langage').onclick=Quitter_Langues;
+}
+
+function Quitter_Langues(){
+    $('#langues').css("visibility","hidden");
+    $('#langage').onclick=Afficher_Langues;
 }
