7b6aa6074d0d38b2f5778274a6e414285266e156
MONFOUGA MARIE <marie.monfouga@gmail.com>
2018-11-21 12:18:41 +0100

    liens local plus précis

--- affichage.js
+++ affichage.js
@@ -1,3 +1,8 @@
+if (typeof(Storage) !== "undefined") {
+    // Store
+    localStorage.setItem("langue", "français");
+}
+
 function AfficherLangues(){
     $('#langues').css("visibility","visible");
     document.getElementById('langage').onclick=QuitterLangues;
@@ -6,4 +11,10 @@ function AfficherLangues(){
 function QuitterLangues(){
     $('#langues').css("visibility","hidden");
     document.getElementById('langage').onclick=AfficherLangues;
+}
+
+function ChangerAnglais(){
+    $('#langue_actuelle').attr("src","ressources/anglais.jpg");
+    $('#langues').css("visibility","hidden");
+    
 }
