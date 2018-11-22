d8565f37b97795b015d4a88d122d61a6ee0a0300
azertui <root@localhost.localdomain>
2018-11-22 19:08:48 +0100

    update

--- jeu.js
+++ jeu.js
@@ -36,7 +36,33 @@ function getMode(){
   return localStorage.getItem("mode");
 }
 
+function ApplyColor(card,color){//card's type: Blue,Red,Grey,Black,hidden
+  let c=#FF4500;
+  if(color=="Blue"){
+  }
+  else if(color=="Red"){
+    c=0xFF3300;
+  }
+  else if(color=="Grey"){
+
+  }
+  else if(color=="Black"){
+
+  }
+  card.css("background", c);
+}
+
 function cardsUpdate(){
+  //On récupère les cartes
+  let CardArray=noyau.getBoardState();
+  let htmlCard=new Array();
+  for(let i=0;i<5;i++){
+     htmlCard[i]=new Array();
+     for(let j=0;j<5;j++){
+        htmlCard[i][j]=document.getElementById("c"+i+j);
+        htmlCard[i][j].innerHTML=CardArray[i][j].getValue();
+     }
+  }
 
 }
 
@@ -60,5 +86,6 @@ window.onload= function(){
   //Création du noyau
   noyau = new Noyau(getMode(), getLangue(), getEquipe1(), getNumberEquipe1(), getEquipe2(), getNumberEquipe2());
   HasCore=1;
+  cardsUpdate();
   console.log("Noyau créé");
 }
