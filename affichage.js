d8e122a4778968fd10493835d5d52eaa801ff94f
Louis <louis.thomann@etu.unistra.fr>
2018-11-23 11:43:22 +0100

    Modif plateau.html modalbox-start. Fixe langue bug et implémentation nettoyage lors d'exit.

--- affichage.js
+++ affichage.js
@@ -1,23 +1,50 @@
 $(document).ready(function () {
-
+	let tmp1, tmp2, tmp3;
+		
 	// Gestion de la barre de son
 	$('#son [data-toggle="popover"]').popover({
 		html : true,
 		trigger : "focus",
 		content: '<div class="input-group"><input id="barre_son" type="range" min="0" max="100" value="50"></div>'
 	});
-
+	
+	// Gestion de la langue
+	tmp1 = $('#langue_actuelle');
+	if (window.localStorage && window.localStorage.hasOwnProperty('langue')) {
+		tmp2 = tmp1.attr('src');
+		tmp3 = localStorage.getItem('langue');
+		if ( tmp2 !== tmp3 ) {
+			tmp1.attr('src', tmp3);
+		}
+	}
+	else {
+		tmp1.attr('src', 'ressources/Drapeau_France.png');
+	}
+	
+	// Gestion de l'exit du jeu
+	$('#exit').click(function() {
+		if (window.localStorage) {
+			for (key in localStorage) {
+				localStorage.removeItem(key);
+			}
+		}
+	});
+	
+	
+	// Gestion modal début plateau.html
+	tmp1 = $('#btn-modal-start');
+	tmp2 = tmp1.text();
+	tmp1.text( tmp2 + noyau.teams[noyau.currentTeam].getColor());
 });
 
 let HasCore=0;
 if (typeof(Storage) !== "undefined") {
     // Store
     if(localStorage.getItem("langue")==null)
-      localStorage.setItem("langue", "Drapeau_France.png");
-    $('#langue_actuelle').attr("src","ressources/"+localStorage.getItem("langue"));
+      localStorage.setItem("langue", "ressources/Drapeau_France.png");
+    $('#langue_actuelle').attr("src",localStorage.getItem("langue"));
 
-    if(localStorage.getItem("couleur_equipe")==null)
-      localStorage.setItem("couleur_equipe", "#4d94ff");
+    if(localStorage.getItem("couleur_equipe")==null) localStorage.setItem("couleur_equipe", "#4d94ff");
     $('.box_tour').css("background",localStorage.getItem("couleur_equipe"));
     if(localStorage.getItem("couleur_equipe2")==null)
       localStorage.setItem("couleur_equipe2", "#ff751a");
@@ -45,21 +72,21 @@ function changeNoyauLang() {
 function ChangerAnglais(){
     $('#langue_actuelle').attr("src","ressources/anglais.jpg");
     $('#langues').css("visibility","hidden");
-    localStorage.setItem("langue", "anglais.jpg");
+    localStorage.setItem("langue", "ressources/anglais.jpg");
     changeLangue();
 }
 
 function ChangerEspagnol(){
     $('#langue_actuelle').attr("src","ressources/espagnol.jpg");
     $('#langues').css("visibility","hidden");
-    localStorage.setItem("langue", "espagnol.jpg");
+    localStorage.setItem("langue", "ressources/espagnol.jpg");
     changeLangue();
 }
 
 function ChangerFrançais(){
     $('#langue_actuelle').attr("src","ressources/Drapeau_France.png");
     $('#langues').css("visibility","hidden");
-    localStorage.setItem("langue", "Drapeau_France.png");
+    localStorage.setItem("langue", "ressources/Drapeau_France.png");
     changeLangue();
 }
 
