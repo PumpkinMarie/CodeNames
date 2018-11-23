2606a7eb5126e531b83c9e3e19d1342304ef1976
Robin Biscay <biscay.rob@gmail.com>
2018-11-23 21:53:12 +0100

    langue correction erreurs

--- langue.js
+++ langue.js
@@ -4,7 +4,7 @@ getWordRandom(ES);
 */
 const FR = 0, EN = 1, ES = 2;
 class Langue {
-	getWord(entries, index, language) {
+	static getWord(entries, index, language) {
 		if (language == FR) {
 			return entries[index][0];
 		} else if (language == EN) {
@@ -14,66 +14,51 @@ class Langue {
 		}
 	}
 
-	getWordIndex(word,lang){
-		let i,j;
-		for(i=0;i<Dico.length;i++){
-			if(lang==FR){
-				if(Dico[i][0]==word)
+	static getWordIndex(word, lang) {
+		let i, j;
+		for (i = 0; i < Dico.length; i++) {
+			if (lang == FR) {
+				if (Dico[i][0] == word)
 					return i;
-			}else if(lang==EN){
-				if(Dico[i][1][0]==word)
+			} else if (lang == EN) {
+				if (Dico[i][1][0] == word)
 					return i;
-			}else{
-				if(Dico[i][1][1]==word)
+			} else {
+				if (Dico[i][1][1] == word)
 					return i;
 			}
 		}
 		return -1;
 	}
 
-	Translatefrom(word,lang,newLang){
-		let ind=getWordIndex(word,lang);
-		if(ind!=-1){
-			if(newLang==FR)
+	static Translatefrom(word, lang, newLang) {
+		let ind = getWordIndex(word, lang);
+		if (ind != -1) {
+			if (newLang == FR)
 				return Dico[ind][0];
-			else if(newLang==EN)
+			else if (newLang == EN)
 				return Dico[ind][1][0];
 			else
 				return Dico[ind][1][1];
 		}
 	}
 
-	getWordRandom() {
-		this.getWordRandom(actuel);
+	static getWordRandom() {
+		getWordRandom(actuel);
 	}
 
-	getWordRandom(language) {
+	static getWordRandom(language) {
 		let entries = Object.entries(Dico);
 		let rand = Math.floor(Math.random() * Math.floor(entries.length));
-		console.log(getWord(entries, rand, EN));
+		return Langue.getWord(entries, rand, EN);
 	}
 
-	isLangueDisponible(langue) {
-		if (langue.is(String)) {
-			return false;
-		}
-		else if (langue.is(Number)) {
-			let bool = 0;
-			for (let key in this.langue) {
-				bool += (this.langue[key] === langue) ? 1 : 0;
-			}
-
-			return (bool > 0);
-		}
-
-		return false;
-	}
 
 	constructor(lang) {
 		if (lang != FR && lang != EN && lang != ES) {
-			this.actuel = FR;
+			actuel = FR;
 		} else {
-			this.actuel = lang;
+			actuel = lang;
 		}
 	}
 }
--- traduction.js
+++ traduction.js
@@ -1,22 +1,22 @@
-const Dico = {
-			"Local": [
-						"Local",
-						"Local"
-			],
-			"Reseau": [
-						"Online",
-						"Red"
-			]
-			"Tutoriel": [
-						"Tuto",
-						"Tuto"
-			]
-			"Competitif": [
-						"Compet",
-						"Compet"
-			]
-			"Agents, préparez-vous pour une bataille acharnée. Votre mission, si toutefois vous choisissez ce mode, est de vaincre l’équipe adverse composée d’autres joueurs. Votre groupe d’ami sera divisé en 2 équipes d’au moins 2 joueurs(1 maitre espion et 1 agent). Bonne chance!": [
-							"Agents, your mission is to defeat the other team. Your friends and you will be divided into 2 teams, each team should have 2 members at least(a spy master and an agent). Good luck! ",
-							"Yolo"
-				]
+const Text = {
+	"Local": [
+		"Local",
+		"Local"
+	],
+	"Reseau": [
+		"Online",
+		"Red"
+	],
+	"Tutoriel": [
+		"Tuto",
+		"Tuto"
+	],
+	"Competitif": [
+		"Compet",
+		"Compet"
+	],
+	"Agents, préparez-vous pour une bataille acharnée. Votre mission, si toutefois vous choisissez ce mode, est de vaincre l’équipe adverse composée d’autres joueurs. Votre groupe d’ami sera divisé en 2 équipes d’au moins 2 joueurs(1 maitre espion et 1 agent). Bonne chance!": [
+		"Agents, your mission is to defeat the other team. Your friends and you will be divided into 2 teams, each team should have 2 members at least(a spy master and an agent). Good luck! ",
+		"Yolo"
+	]
 };
