a1384054f4403d5dfd7c6f2be9c468c5adebe4fe
Louis <louis.thomann@etu.unistra.fr>
2018-11-23 17:47:45 +0100

    Implémentation d'un modalbox dans plateau.html Adaptation du background pour mobile Adaptation de la navbar avec un burger pour mobile

--- affichage.js
+++ affichage.js
@@ -1,41 +1,3 @@
-$(document).ready(function () {
-	let tmp1, tmp2, tmp3;
-		
-	// Gestion de la barre de son
-	$('#son [data-toggle="popover"]').popover({
-		html : true,
-		trigger : "focus",
-		content: '<div class="input-group"><input id="barre_son" type="range" min="0" max="100" value="50"></div>'
-	});
-	
-	// Gestion de la langue
-	tmp1 = $('#langue_actuelle');
-	if (window.localStorage && window.localStorage.hasOwnProperty('langue')) {
-		tmp2 = tmp1.attr('src');
-		tmp3 = localStorage.getItem('langue');
-		if ( tmp2 !== tmp3 ) {
-			tmp1.attr('src', tmp3);
-		}
-	}
-	else {
-		tmp1.attr('src', 'ressources/Drapeau_France.png');
-	}
-	
-	// Gestion de l'exit du jeu
-	$('#exit').click(function() {
-		if (window.localStorage) {
-			for (key in localStorage) {
-				localStorage.removeItem(key);
-			}
-		}
-	});
-	
-	
-	// Gestion modal début plateau.html
-	tmp1 = $('#btn-modal-start');
-	tmp2 = tmp1.text();
-	tmp1.text( tmp2 + noyau.teams[noyau.currentTeam].getColor());
-});
 
 let HasCore=0;
 if (typeof(Storage) !== "undefined") {
@@ -70,23 +32,23 @@ function changeNoyauLang() {
 }
 
 function ChangerAnglais(){
-    $('#langue_actuelle').attr("src","ressources/anglais.jpg");
+    $('#langue_actuelle').attr("src","./ressources/anglais.jpg");
     $('#langues').css("visibility","hidden");
-    localStorage.setItem("langue", "ressources/anglais.jpg");
+    localStorage.setItem("langue", "./ressources/anglais.jpg");
     changeLangue();
 }
 
 function ChangerEspagnol(){
-    $('#langue_actuelle').attr("src","ressources/espagnol.jpg");
+    $('#langue_actuelle').attr("src","./ressources/espagnol.jpg");
     $('#langues').css("visibility","hidden");
-    localStorage.setItem("langue", "ressources/espagnol.jpg");
+    localStorage.setItem("langue", "./ressources/espagnol.jpg");
     changeLangue();
 }
 
 function ChangerFrançais(){
-    $('#langue_actuelle').attr("src","ressources/Drapeau_France.png");
+    $('#langue_actuelle').attr("src","./ressources/Drapeau_France.png");
     $('#langues').css("visibility","hidden");
-    localStorage.setItem("langue", "ressources/Drapeau_France.png");
+    localStorage.setItem("langue", "./ressources/Drapeau_France.png");
     changeLangue();
 }
 
@@ -178,3 +140,45 @@ function banniereFJ (nom) {
 	 }
 	 $('#tour').css("visibility", "visible");
 }
+
+$(document).ready(function () {
+	let tmp1, tmp2, tmp3;
+	
+	// Gestion de la barre de son
+	$('#son [data-toggle="popover"]').popover({
+		html : true,
+		trigger : "focus",
+		content: '<div class="input-group"><input id="barre_son" type="range" min="0" max="100" value="50"></div>'
+	});
+	
+	// Gestion de la langue
+	tmp1 = $('#langue_actuelle');
+	if (window.localStorage && window.localStorage.hasOwnProperty('langue')) {
+		tmp2 = tmp1.attr('src');
+		tmp3 = localStorage.getItem('langue');
+		if ( tmp2 !== tmp3 ) {
+			tmp1.attr('src', tmp3);
+		}
+	}
+	else {
+		tmp1.attr('src', './ressources/Drapeau_France.png');
+	}
+	
+	// Gestion de l'exit du jeu
+	$('#exit').click(function() {
+		if (window.localStorage) {
+			for (key in localStorage) {
+				localStorage.removeItem(key);
+			}
+		}
+	});
+	
+	// Gestion modal début plateau.html
+	tmp1 = $('#plateau-start');
+	tmp1.on("show.bs.modal", function() {
+		// noyau.teams[noyau.currentTeam].getColor()
+		$('#btn-modal-start').text( "Je suis le maître espion " + "bleu")
+			.addClass("btn-primary");
+	});
+	// tmp1.modal({show : true, backdrop: "static"});
+});
